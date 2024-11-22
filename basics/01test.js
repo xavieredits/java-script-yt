9eb2ca38cf8831d16299ba38696d9f685bacda15
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-22 16:36:17 +0000

    conversions

---
+++ basics/01test.js
@@ -0,0 +1,14 @@
+const accountId = 233
+let accountMail = "xav@gmail"
+var accountCity ="canda"
+accountPass ="443"
+// here if try to change the vaule of cont datatype it is not possible 
+//accountId = 33
+accountCity ="london"
+accountPass="444"
+accountMail="Cav@gmail"
+
+console.table([accountCity,accountId,accountMail,accountPass]);
+
+// we use .table command to retrive info in table form
+// let is more preferd the var cause of block scope and fucntinal scope {} issues 
---
+++ basics/03conversionoperation.js
@@ -0,0 +1,12 @@
+let score = 33
+
+console.log(typeof score);// number
+console.log(typeof(score));// another method of knowing type and writing code output is same
+
+// if value of score is writen in  ":" then string
+let scr ="33"
+console.log(typeof(scr));// string
+
+// converting data type
+let valueInNumber =Number(scr) // converts into numeber
+

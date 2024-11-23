b459261bfe5ea301075803e11b9f8b6f90cbb1f8
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-23 11:51:59 +0000

    conversions and operations

--- basics/03conversionoperation.js
+++ basics/03conversionoperation.js
@@ -26,3 +26,50 @@ console.log(boolenIsLoggedIn);
 // true for 1 false for 0 
 // for empty strings "" value is false
 // for strings "xani" value is true
+
+// ****************************************** OPERATIONS *********************************************************************
+ let value= 3
+ let negValue =-value
+ console.log(negValue); // -3 negative value
+
+ console.log(2+2);// addition
+ console.log(2-2);// subtraction
+ console.log(2*2);// multiplication
+ console.log(2**2);// power
+ console.log(2/2);// division
+ console.log(2%2); // remainder (use in cryptography and stocks software)
+
+//  joining two strings
+
+let str1 ="hello"
+let str2 = "world"
+let str3 = str1 + str2
+console.log(str3);
+
+//  real problem 
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+2 + 2);//122
+console.log(1+2 + "2");//32
+//  all these different values and output cause due to some guidliness and rules in javascrpit can be found in ecma
+// to solve these kind of issues prentheses "()" to create better code example:
+console.log((3+4) *5%8);//3
+
+// trickey conversions
+
+console.log(+true);//is 1 cause true itself is a boolean
+console.log(+"");// is 0 cause "" is itself a 0 and use for boolean 
+// but
+// console.log(true+);// not possible and will give error
+
+let num1 ,num2 , num3
+num1 = num2 = num3 = 2+2 // bad redibility and wrong way to assign value
+
+let gameCounter =100
+gameCounter++; // or ++gameCounter                  // prefix and postfix operator
+console.log(gameCounter);
+
+ 
+
+

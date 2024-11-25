3890b336c62b3e01fa66179a5e87ab2e7d0c09b4
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-25 16:25:16 +0000

    numbers_and_math

---
+++ basics/06_numbers_and_maths.js
@@ -0,0 +1,23 @@
+const score = 400 // here javaScrpit is automatically convert to number datatype
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));// used in  ecomerce webs and other apps for precision value {adds decimal}
+
+const otherNumber =23.85533 //23.9
+console.log(otherNumber.toPrecision(3));// rounds off to given value
+
+// another example
+
+const antoherNumber =123.88945 // 124
+console.log(antoherNumber.toPrecision(3));
+
+
+
+
+
+

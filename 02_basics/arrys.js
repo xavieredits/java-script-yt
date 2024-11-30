196ba44cdbc32c133467de3211a80fc60d59d14f
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-30 17:52:43 +0000

    arrys

---
+++ 02_basics/arrys.js
@@ -0,0 +1,21 @@
+// ARRYS
+
+const myArr= [ 0,1,2,3,4,5,6]
+//           {   Elements    } 
+//  arry is an object which can contain mixture of datat and various values 
+
+console.log(myArr[3]);
+
+//  to know arryas create a shallow copy which means it's a copy which share the same references as those of the source 
+// (in simpe words) the copy has the original source as the reference given 
+
+// On the other hand deep copies does not share the same references as the same of the source
+
+const myHers = ["marvle hers", " dc hers" ," the boys series" , " other unkonwn examplse",]
+console.log(myHers.length);
+console.log(myHers[2]);
+
+// ARRY MEHTODS
+
+myArr.push(6);
+console.log(myArr);

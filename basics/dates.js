07ce0d1be576ddc1ea8a135113f0f4d9c2d96968
xavier_edits <165995012+xavieredits@users.noreply.github.com>
2024-11-30 16:49:38 +0000

    dates

--- basics/dates.js
+++ basics/dates.js
@@ -20,8 +20,34 @@ console.log(typeof(myDate));// object
 let myCreatedDate = new Date(2024 , 0 , 23)
 console.log(myCreatedDate); //2024-01-23T00:00:00.000Z
 console.log(myCreatedDate.toString());//Tue Jan 23 2024 00:00:00 GMT+0000
-//  note months starts from 0 in javascript
+//  note : months starts from 0 in javascript
 
 
+// adding time with dates FORMAT
 
+let anotherDate = new Date(2023 , 0 , 23 , 5 ,3)
+console.log(anotherDate.toString());// Mon Jan 23 2023 05:03:00 GMT+0000  time
+
+// another method of writing date is dd/ mm /yy or yy/ mm /dd
+
+// EXAMPLE
+let currentDate = new Date("01-11-2023")
+let pastDate = new Date ("2024-7-23");
+console.log(`currentDate:${currentDate} and pastdat ${pastDate}`);
+
+let mytepDate = Date.now()
+console.log(mytepDate);// gives in millis sec
+
+console.log(myCreatedDate.getTime());// to get time from any kind of date and time stamp and convert it to milli sec 
+
+console.log(Math.floor(Date.now()/1000));// give more precise value for comparision and assumptions 
+// simple world small value
+
+let newDate = new Date() // note: date is object
+console.log(newDate);// 2024-11-30T16:46:44.789Z (output)
+//  more functions for date 
+console.log(newDate.getMonth());//10
+console.log(newDate.getDay());//6
+console.log(newDate.getFullYear());//2024
+console.log(newDate.getTime());//1732985312906 in milli sec
 
